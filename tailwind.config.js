e4e629ef8b91af0e44ec02fd42bffb01b6a8f5dc
Steve Bourgoin <stiv.aw@gmail.com>
2021-01-12 20:21:20 -0500

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  corePlugins: {
+    preflight: false,
+  },
+  purge: ["./imports/ui/**/*.tsx"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
